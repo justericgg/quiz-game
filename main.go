82cea51a41d90ce85844242afbf9963377b092e6
Eric G. Huang <eric.g.yuan@gmail.com>
2019-08-15 21:04:39 +0800

    Add timer for the quiz game

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 
 	fileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	limitSec := flag.Int("limit", 30, "a time limit for the quiz game in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*fileName)
@@ -28,23 +30,55 @@ func main() {
 
 	problems := parseLine(lines)
 
+	timer := time.NewTimer(time.Duration(*limitSec) * time.Second)
+
 	correct := 0
 
 	for i, p := range problems {
-		fmt.Printf("problem#%d: %s\n", i+1, p.question)
-		var userAnswer string
-		_, err := fmt.Scanln(&userAnswer)
-		if err != nil && err.Error() != "unexpected newline" {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		fmt.Printf("problem#%d: %s = ", i+1, p.question)
+		userAnswerChan := answerQuiz()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case userAnswer := <-userAnswerChan:
 
-		if userAnswer == p.answer {
-			correct++
+			if userAnswer.Error != nil && userAnswer.Error.Error() != "unexpected newline" {
+				fmt.Println("exception occur")
+				os.Exit(1)
+			}
+
+			if userAnswer.Answer == p.answer {
+				correct++
+			}
 		}
 	}
 
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+type UserAnswer struct {
+	Error  error
+	Answer string
+}
+
+func answerQuiz() <-chan UserAnswer {
+	answerChan := make(chan UserAnswer)
+
+	go func() {
+		defer close(answerChan)
+
+		var userAnswer string
+		_, err := fmt.Scanln(&userAnswer)
+
+		answer := UserAnswer{
+			Error:  err,
+			Answer: userAnswer,
+		}
+		answerChan <- answer
+	}()
+
+	return answerChan
 }
 
 func parseLine(lines [][]string) []problem {
